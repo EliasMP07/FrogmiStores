4a628caf1192212a72941e13e97ee59d1218a2f0
elias <eliasmenapech@gmail.com>
2024-06-12 19:29:11 -0600

    Implement Material Theme

---
+++ app/src/main/java/com/example/frogmistores/core/presentation/designsystem/Icon.kt
@@ -0,0 +1,19 @@
+package com.example.frogmistores.core.presentation.designsystem
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.example.frogmistores.R
+
+val StoreIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.store_icon)
+
+val Logo: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.logo)
+
+val LocationStore: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.locatoonstore)
--- app/src/main/java/com/example/frogmistores/core/presentation/designsystem/Type.kt
+++ app/src/main/java/com/example/frogmistores/core/presentation/designsystem/Type.kt
@@ -2,33 +2,67 @@ package com.example.frogmistores.core.presentation.designsystem
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import com.example.frogmistores.R
+
+val ValeraRound = FontFamily(
+    Font(
+        resId = R.font.valera_round,
+        weight = FontWeight.Light
+    ),
+    Font(
+        resId = R.font.valera_round,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resId = R.font.valera_round,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resId = R.font.valera_round,
+        weight = FontWeight.SemiBold
+    ),
+    Font(
+        resId = R.font.valera_round,
+        weight = FontWeight.Bold
+    ),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    bodySmall = TextStyle(
+        fontFamily = ValeraRound,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 20.sp,
+        textAlign = TextAlign.Justify
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = ValeraRound,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = ValeraRound,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.5.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    labelLarge = TextStyle(
+        fontFamily = ValeraRound,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 24.sp,
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = ValeraRound,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+    ),
 )
