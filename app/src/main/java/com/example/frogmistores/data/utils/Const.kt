08132f8022bd8d19fa54d34792924c89ea89802a
elias <eliasmenapech@gmail.com>
2024-06-13 15:10:26 -0600

    Remove Credentials

--- app/src/main/java/com/example/frogmistores/data/utils/Const.kt
+++ app/src/main/java/com/example/frogmistores/data/utils/Const.kt
@@ -5,7 +5,7 @@ package com.example.frogmistores.data.utils
  * Contiene valores fijos que se utilizan en diferentes partes del código.
  */
 object Const {
-    const val COMPANY_UUID = "b7fa583e-a144-4ec2-9464-e1e514512fb4" //Pega aqui tus credenciales
-    const val KEY = "bc27271a27527aaf6126c781dd17e7dd" //Pega aqui tus credenciales
+    const val COMPANY_UUID = "" //Pega aqui tus credenciales
+    const val KEY = "" //Pega aqui tus credenciales
     const val ITEMS_PER_PAGE = 10 //Tamaño de los items de la paginacion
 }
